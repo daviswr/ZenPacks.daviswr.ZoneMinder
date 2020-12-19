49b34ec8ebe1a1e3dc72cff675148bc7558692c0
Wes Davis <wesley.r.davis@gmail.com>
2020-12-19 15:31:04 -0500

    UPD: Version to 0.9.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.daviswr.ZoneMinder"
-VERSION = "0.7.0"
+VERSION = "0.9.0"
 AUTHOR = "Wes Davis"
 LICENSE = "MIT"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.daviswr']
